52b931b49c478667fe2aa749f5a28a49a21156a3
Gina Peng <ginapeng2002@gmail.com>
2025-08-07 21:37:33 -0500

    Fix GitHub Pages routing with basename and 404.html fallback

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ const ScrollToTop = () => {
 
 function App() {
   return (
-    <Router>
+    <Router basename="/yumemi-website">
       <ScrollToTop />
       <div className="App">
         <Header />
